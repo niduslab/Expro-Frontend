7b41b9e7f5de82a3ccd955104a082c7fedb341dc
kik-work <kik.work100@gmail.com>
2026-03-08 09:08:21 +0600

    updated with responsiveness

--- app/(public)/donate/page.tsx
+++ app/(public)/donate/page.tsx
@@ -11,8 +11,9 @@ const Donation = () => {
       <Hero />
 
       <section className="py-16  overflow-hidden">
-        <div className="max-w-7xl mx-auto px-6 md:px-10 lg:px-16">
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-16 items-start">
+        <div className="w-full max-w-[1440px] 2xl:max-w-[1548px] mx-auto px-6 md:px-10 lg:px-20 xl:px-36 2xl:px-18">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-8 xl:gap-10 sm:px-2 lg:px-0 xl:px-4 items-start">
+            {" "}
             {/* Image */}
             <div className="w-full">
               <div className="relative w-full aspect-[4/5] rounded-3xl overflow-hidden shadow-lg">
@@ -26,7 +27,6 @@ const Donation = () => {
                 />
               </div>
             </div>
-
             {/* Content */}
             <div>
               <div className="mb-8">
@@ -96,7 +96,7 @@ const Donation = () => {
           </div>
         </div>
       </section>
-      <section className="space-y-8 px-8  lg:px-20 ">
+      <section className="space-y-8 px-6  w-full max-w-[1440px] 2xl:max-w-[1548px] mx-auto md:px-12 lg:px-20  xl:px-40 2xl:px-22  ">
         {/* Heading */}
         <div>
           <h3 className="text-3xl md:text-4xl font-semibold text-gray-900">
@@ -154,7 +154,7 @@ const Donation = () => {
           </p>
         </div>
       </section>
-      <section>
+      <section className="w-full max-w-[1440px] 2xl:max-w-[1548px] mx-auto mb-6 px-6 md:px-12 lg:px-20 xl:px-40 2xl:px-22">
         <Donationform />
       </section>
     </>
