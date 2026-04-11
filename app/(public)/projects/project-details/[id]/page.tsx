4726281a5716c3396d6b1f875a41d54999b7fe50
kik-work <kik.work100@gmail.com>
2026-04-11 17:38:03 +0600

    udpated individual project details displaying

---
+++ app/(public)/projects/project-details/[id]/page.tsx
@@ -0,0 +1,13 @@
+import ProjectBody from "./body";
+import Hero from "./hero";
+
+const ProjectDetailsPage = () => {
+  return (
+    <>
+      <Hero />
+      <ProjectBody />
+    </>
+  );
+};
+
+export default ProjectDetailsPage;
